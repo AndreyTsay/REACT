a8b6442ec227ab599316a3387b3796a07a8aacdf
TsayAndrey <skaeris@yandex.ru>
2024-06-14 10:10:04 +0300

    Add some things(validate forms)

--- first-react/src/components/JournalForm/JournalForm.jsx
+++ first-react/src/components/JournalForm/JournalForm.jsx
@@ -1,33 +1,58 @@
 // import React from 'react';
-import { useState } from 'react';
-import './JournalForm.css';
+import styles from './JournalForm.module.css';
 import Button from '../Button/Button';
+import { useState } from 'react';
+import cn from 'classnames';
 
-function JournalForm() {
-	const [inputData, setInputData] = useState('');
+function JournalForm({onSubmit}) {
 
-	const inputChange = (event) => {
-		setInputData(event.target.value); 
-		console.log(inputData);
-	};
+	const [formValidState, setFormValidState] = useState({
+		title: true,
+		post: true,
+		date: true
+	});
     
 	const addJournalItem = (e) => {
 		e.preventDefault();
 		const formData = new FormData(e.target);
 		const formProps = Object.fromEntries(formData);
-		console.log(formProps);
+		let isFormValid = true;
+		if (!formProps.title?.trim().length) {
+			setFormValidState(state => ({...state,title:false}));
+			isFormValid = false;
+		} else {
+			setFormValidState(state => ({...state,title:true}));
+		}
+		if (!formProps.post?.trim().length) {
+			setFormValidState(state => ({...state,post:false}));
+			isFormValid = false;
+		} else {
+			setFormValidState(state => ({...state,post:true}));
+		}
+		if (!formProps.date) {
+			setFormValidState(state => ({...state,date:false}));
+			isFormValid = false;
+		} else {
+			setFormValidState(state => ({...state,date:true}));
+		}
+		if (!isFormValid) {
+			return;
+		}
+		onSubmit(formProps);
 	};
 
 	return (
-		<form className='jornal-form' onSubmit={addJournalItem}>
-			<input type='text' name='title'/>
-			<input type='date' name='date'/>
-			<input type='text'name='tag' value={inputData} onChange={inputChange}/>
-			<textarea name="post" id="" cols='30' rows='10'></textarea>
-			<Button text={'Сохранить'} onClick={() => {console.log('Нажали');}}/>
+		<form className={styles['jornal-form']} onSubmit={addJournalItem}>
+			<input type='text' name='title' className={cn(styles['input'],{
+				[styles['invalid']] : !formValidState.title
+			})}/>
+			<input type='date' name='date'className={`${styles['input']} ${formValidState.date ? '' : styles['invalid']}`}/> 
+			<input type='text'name='tag' />
+			<textarea name="post" id="" cols='30' rows='10' className={`${styles['input']} ${formValidState.post ? '' : styles['invalid']}`}> </textarea>
+			<Button text={'Сохранить'} />
 		</form>
 
-	);
+	); 
 }
 
 export default JournalForm;
--- first-react/src/components/JournalList/JournalList.jsx
+++ first-react/src/components/JournalList/JournalList.jsx
@@ -1,11 +1,37 @@
 import './JournalList.css';
+import CardButton from '../CardButton/CardButton';
+import Journalitem from '../Journalitem/Journalitem';
 
-function JournalList({children}) {
+function JournalList({items}) {
+	if(items.length ===0) {
+		return <p>Записей еще нет, добавьте первую</p>;
+	}
+	const sortItems = (a, b) => {
+		if (a.date < b.date) {
+			return 1;
+		}
+		else {
+			return - 1;
+		}
+	};
 
-	return (
-		<div className='journal-list'>{children}</div>
-
-	);
+	return <>{items.sort(sortItems).map(
+		el => ( 
+			<CardButton key ={el.id}>
+				<Journalitem
+					title={el.title}
+					text={el.text}
+					date={el.date}
+				/>
+			</CardButton>
+		))}
+	</>;
 }
 
-export default JournalList;
+// 	return (
+// 		<div className='journal-list'>{item}</div>
+
+// 	);
+
+
+export default JournalList; 
